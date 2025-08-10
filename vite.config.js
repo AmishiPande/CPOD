e8666888ed7411858c544243186313c0860d248b
Amishi Pande <165027208+AmishiPande@users.noreply.github.com>
2025-08-11 00:32:12 +0530

    Updated path to index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CPOD/'
+  base: '/CPOD/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'public/index.html')
+      }
+    }
+  }
 })
