5db3e48831fb0e3dcc573c264ddedef6534e1b09
Amishi Pande <165027208+AmishiPande@users.noreply.github.com>
2025-08-10 18:26:03 +0530

    Updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <p>&copy; {new Date().getFullYear()} Space Pictures. Inspired by NASA APOD.</p>
+        <p>CPOD. Inspired by <a href="https://apod.nasa.gov/apod/astropix.html">APOD</a>.
+          <br><h5>&copy; {new Date().getFullYear()} A race of cat-human-hybrid-astrology-hating-astronomy-plus-physics-freaks.</h5>
+          <br><h6>This is not a real copyright tho-</h6></p>
       </div>
     </footer>
   );
