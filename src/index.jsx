6251ec624dfd0d4f042de2d07315075c2008b807
Amishi Pande <165027208+AmishiPande@users.noreply.github.com>
2025-08-12 01:14:44 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../../css/styles.css';
-import App from './App.jsx';  // Updated extension
+import './css/styles.css';
+import App from './App.jsx'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
