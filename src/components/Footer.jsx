38744bb070bf16596fb8e0affe2f7e3c14274abb
Amishi Pande <165027208+AmishiPande@users.noreply.github.com>
2025-08-12 01:43:46 +0530

    fixed Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,13 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <p>CPOD. Inspired by <a href="https://apod.nasa.gov/apod/astropix.html">APOD</a>.
-          <br><h5>&copy; {new Date().getFullYear()} A race of cat-human-hybrid-astrology-hating-astronomy-plus-physics-freaks.</h5>
-          <br><h6>This is not a real copyright tho-</h6></p>
+        <p>
+          CPOD. Inspired by <a href="https://apod.nasa.gov/apod/astropix.html">APOD</a>.
+        </p>
+        <h5>
+          &copy; {new Date().getFullYear()} A race of cat-human-hybrid-astrology-hating-astronomy-plus-physics-freaks.
+        </h5>
+        <h6>This is not a real copyright tho-</h6>
       </div>
     </footer>
   );
